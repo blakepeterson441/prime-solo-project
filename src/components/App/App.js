a79f854eb4dd7bab6195ebc61d93d56f5bae7b86
Blake <blake.peterson.441@gmail.com>
2019-08-06 10:34:52 -0500

    changed userpage to profile page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
 import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
+import ProfilePage from '../ProfilePage/ProfilePage';
 import InfoPage from '../InfoPage/InfoPage';
 
 import './App.css';
@@ -30,8 +30,8 @@ class App extends Component {
         <div>
           <Nav />
           <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
+            {/* Visiting localhost:3000 will redirect to localhost:3000/profile */}
+            <Redirect exact from="/" to="/profile" />
             {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
             <Route
@@ -40,13 +40,13 @@ class App extends Component {
               component={AboutPage}
             />
             {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/home will show the UserPage if the user is logged in.
+            Visiting localhost:3000/profile will show the ProfilePage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/home */}
+            Even though it seems like they are different pages, the user is always on localhost:3000/profile */}
             <ProtectedRoute
               exact
-              path="/home"
-              component={UserPage}
+              path="/profile"
+              component={ProfilePage}
             />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
