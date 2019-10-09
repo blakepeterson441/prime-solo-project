9876122e119b560cac2a922932ca6f1b3094950c
Blake <blake.peterson.441@gmail.com>
2019-10-08 19:35:54 -0500

    tidied up getFriends router

--- server/routes/getFriends.router.js
+++ server/routes/getFriends.router.js
@@ -6,11 +6,11 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('GET friends server', req.user);
   const sqlText = `(select username, friends.approved, friends.user_id_two as friend_ID from "users" 
-                        join friends on friends.user_id_two = "users".id where 
-                        friends.user_id_one = $1)
+                        join friends on friends.user_id_two = "users".id 
+                        where friends.user_id_one = $1)
                         union
-                        (select username, friends.approved, friends.user_id_one as friend_ID from 
-                        "users" join friends on friends.user_id_one = "users".id 
+                        (select username, friends.approved, friends.user_id_one as friend_ID from "users" 
+                        join friends on friends.user_id_one = "users".id 
                         where friends.user_id_two = $1);`
   const sqlValues = [req.user.id]
   pool.query(sqlText, sqlValues)
